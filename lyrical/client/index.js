d4d5361901c2ebf875a8e4fb68d7201557e21b1c
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-07-31 14:39:16 -0300

    chore: Add react-router.

--- lyrical/client/index.js
+++ lyrical/client/index.js
@@ -2,6 +2,8 @@ import ApolloClient from 'apollo-client';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
+import { hashHistory, IndexRoute, Route, Router } from 'react-router';
+import App from './components/App';
 import SongList from './components/SongList';
 
 const client = new ApolloClient({});
@@ -9,7 +11,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
