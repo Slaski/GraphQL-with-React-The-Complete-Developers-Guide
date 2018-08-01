0b4796fedba8395e6ceb3b2b4eaee7d94d771243
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-01 16:59:41 -0300

    feat: Add a LyricList component to visualize the lyrics from a song.

--- lyrical/client/components/SongDetail.js
+++ lyrical/client/components/SongDetail.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   render() {
@@ -16,6 +17,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
@@ -27,6 +29,10 @@ const query = gql`
     song(id: $id) {
       id
       title
+      lyrics {
+        id
+        content
+      }
     }
   }
 `;
