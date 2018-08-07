bd09c54efb322f87a1984e8b66483f84643844d8
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-07 10:44:07 -0300

    feat: Add a LoginForm and AuthForm components.

--- auth-graphql/client/index.js
+++ auth-graphql/client/index.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import { hashHistory, Route, Router } from 'react-router';
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id,
@@ -19,7 +20,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App} />
+        <Route path="/" component={App}>
+          <Route path="login" component={LoginForm} />
+        </Route>
       </Router>
     </ApolloProvider>
   );
